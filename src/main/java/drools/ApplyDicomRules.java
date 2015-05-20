14889f57e05238e02ab1866c5804d98c1b2c81bd
dnjiao <oscarjiao@gmail.com>
2015-05-20 17:02:43 -0500

    changes

---
+++ src/main/java/drools/ApplyDicomRules.java
@@ -0,0 +1,27 @@
+package drools;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+
+public class ApplyDicomRules {
+	public static void main(String[] args) {
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newClassPathResource("basic.drl"), ResourceType.DRL);
+		KnowledgeBuilderErrors errors = kbuilder.getErrors();
+		if (errors.size() > 0) {
+			for (KnowledgeBuilderError error: errors) {
+				System.err.println(error);
+			}
+			throw new IllegalArgumentException("Could not parse knowledge.");
+		}
+		KnowledgeBase kbase;
+		kbase = KnowledgeBaseFactory.newKnowledgeBase();
+		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+	}
+}
--- src/main/java/drools/DicomImage.java
+++ src/main/java/drools/DicomImage.java
@@ -3,23 +3,44 @@ package drools;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class DicomImage {
+	String fileName;
 	String patientName;
 	String modality;
-	boolean boolName;
+	boolean boolName = false;
 	
-	public DicomImage (String filename) {
-		FileInputStream fstream = new FileInputStream(filename);
-		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-		String strLine;
-		while ((strLine = br.readLine()) != null) {
-			
+	public DicomImage (File file) {
+		this.fileName = file.getName();
+		FileInputStream fstream;
+		try {
+			fstream = new FileInputStream(file.getAbsolutePath());
+			BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				if (strLine.startsWith("Patient_Name")) {
+					this.patientName = strLine.split("\t")[1];
+					this.boolName = true;
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	public void setPatientName(String str) {
-		
+	public String getFileName() {
+		return fileName;
+	}
+	public String getPatientName() {
+		return patientName;
+	}
+	public boolean getBoolName() {
+		return boolName;
 	}
 	
 }
